398a340f66b2ea5d0fa72cc8a36d6b928cd2af5e
jhansy29 <82075770+jhansy29@users.noreply.github.com>
2021-06-03 00:09:43 +0530

    solution

--- wallis.c
+++ wallis.c
@@ -25,10 +25,13 @@ int main(void) {
 }
 
 float  wallis_pi(int n){
-  int a,n,x=1;
-  for(a=0 ; a<n ; a++){
+  int a,n,x=2;
+  for(a=1 ; a<n ; a++){
     pi = (4*a*a) / (4*a*a -1);
     x=x*pi;
+  }
+  return(x);
+}
     
     
   
